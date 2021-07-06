694f2f459a65d87b57916f2fac1fcef448061334
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-06 13:07:39 -0500

    Start Stores Model

---
+++ Store-Console-App/Models/Store.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace Models
+{
+    public class Store
+    {
+
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Contact { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
