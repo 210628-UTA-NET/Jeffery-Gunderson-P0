25c875dd230f8bcb5ba50686e593ec5176aec1d4
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-06 14:12:08 -0500

    Added Invertory Model

---
+++ Store-Console-App/Models/Inventory.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Models
+{
+    public class Inventory
+    {
+        public int ID { get; set; }
+        public Products Product { get; set; }
+        public int Count { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
+
